6ccec7dbd118c2ab5d9decbd52e22256db599869
matic.vrscaj <matic.vrscaj@gmail.com>
2018-06-05 00:56:10 +0200

    Import entities fix, many minor updates, thumbs support, files

--- resources/views/fe/details/entity.blade.php
+++ resources/views/fe/details/entity.blade.php
@@ -127,7 +127,7 @@
             <div class="accordion" for="accordionMets">
                 Vsi metapodatki
             </div>
-            <div class="accordionContent" id="accordionMets">
+            <div class="accordionContent allMetadata" id="accordionMets">
                 <pre>{{ print_r($data["xml"], true) }}</pre>
             </div>
 
