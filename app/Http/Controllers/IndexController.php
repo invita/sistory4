cfe71d448eb765a6dc951d868e167e2a604fd6bd
matic.vrscaj <matic.vrscaj@gmail.com>
2018-07-02 21:40:55 +0200

    Update, index page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Helpers\DcHelpers;
+use App\Helpers\ElasticHelpers;
 use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
@@ -11,8 +13,21 @@ class IndexController extends FrontendController
 
         $data = [
             "layoutData" => $this->layoutData($request),
+            "indexEntities" => $this->prepareIndexEntities(),
         ];
 
         return view('fe.index', $data);
+    }
+
+
+    private function prepareIndexEntities() {
+        $topMenuHandle = ElasticHelpers::getTopMenuHandleId();
+        $assocData = ElasticHelpers::searchChildren($topMenuHandle);
+        $result = [];
+        foreach ($assocData as $doc) {
+            $result[] = DcHelpers::mapElasticEntity($doc);
+        }
+
+        return $result;
     }
 }
--- resources/views/fe/index.blade.php
+++ resources/views/fe/index.blade.php
@@ -6,8 +6,45 @@
 @section("body")
 <div class="content">
     <div class="row">
-        <div class="medium-12 columns flex flexBothCenter mt-1">
-            Welcome to Si4!
+        <div class="medium-12 columns">
+            <?php /* print_r($indexEntities); */ ?>
+
+
+            <div class="detailsContent">
+                <div class="contentWrap">
+
+                    <div class="detailsDcField detailsDcTitle">
+                        <h3>Welcome to Si4</h3>
+                    </div>
+
+                    <div class="detailsDcField detailsDcDescription">
+                        <p>Where history is stored!</p>
+                    </div>
+
+                    <div class="childrenWrap">
+
+                        @foreach($indexEntities as $indexEntity)
+                            <a href="/details/{{ $indexEntity["handle_id"] }}">
+                                <div class="child">
+                                    <div class="childThumb">
+                                        <img src="{{ $indexEntity["defaultThumb"] }}" />
+                                    </div>
+
+                                    <div class="childTitle">
+                                        <h5>{{ $indexEntity["first_dc_title"] }}</h5>
+                                    </div>
+                                    <div class="childDetails">
+                                        <span class="childCreators"><?php echo $indexEntity["html_dc_creator_list"]; ?></span>
+                                    </div>
+                                </div>
+                            </a>
+                        @endforeach
+                    </div>
+
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
