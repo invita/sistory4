95e420114686e69a8272be8c9ece50053e0086f9
matic.vrscaj <matic.vrscaj@gmail.com>
2019-02-04 19:54:42 +0100

    Cleanup

--- app/Models/OAI/OAIHelper.php
+++ app/Models/OAI/OAIHelper.php
@@ -14,42 +14,6 @@ class OAIHelper {
     private static $allowedArgumentList = array('verb', 'from', 'until', 'metadataPrefix', 'set',
         'cursor', 'resumptionToken', 'batchSize', 'DBGSESSID', 'identifier');
 
-    /*
-    private static $metadataPrefixList = array(
-        'oai_dc' => array(
-            'prefix' => 'oai_dc',
-            'attributes' => [
-                'xmlns' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-                'xmlns:dc' => 'http://purl.org/dc/elements/1.1/',
-                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-                'xsi:schemaLocation' => 'http://www.openarchives.org/OAI/2.0/oai_dc.xsd'
-            ],
-            'handler' => \App\Models\OAI\MetadataPrefix\OAI_SI4::class,
-        ),
-        'oai_datacite' => array(
-            'prefix' => 'oai_datacite',
-            'attributes' => [
-                'xmlns' => 'http://datacite.org/schema/kernel-4',
-                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-                'xsi:schemaLocation' => 'http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd'
-            ],
-            'handler' => \App\Models\OAI\MetadataPrefix\OAI_SI4::class,
-        ),
-    );
-    */
-
-
-    /*
-    public static $identifyInfo = array(
-        "repositoryName" => "Sistory.si OAI Repository",
-        "baseURL" => "http://www.sistory.si/oai.php",
-        "protocolVersion" => "2.0",
-        "adminEmail" => "gregor@invita.si",
-        "earliestDatestamp" => "2011-08-01T00:00:00Z",
-        "deletedRecord" => "no",
-        "granularity" => "YYYY-MM-DDThh:mm:ssZ"
-    );
-    */
 
     public static $defaultBatchSize = 100;
     public static $resumpTokenValidHours = 24;
@@ -59,14 +23,6 @@ class OAIHelper {
     public static function getAllowedVerbList() { return self::$allowedVerbList; }
     public static function getListRequestVerbs() { return self::$listRequestVerbs; }
     public static function getFieldList() { return self::$fieldList; }
-
-    /*
-    public static function getMetadataPrefixList() { return self::$metadataPrefixList; }
-    public static function getMetadataPrefix($mdPrefix) {
-        if (!$mdPrefix || !isset(self::$metadataPrefixList[$mdPrefix])) return null;
-        return self::$metadataPrefixList[$mdPrefix];
-    }
-    */
 
     public static function error($request, $oaiErrorCode, $message) {
         throw new OAIError($request, $oaiErrorCode, $message);
