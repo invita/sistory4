a3276c94105d8cd6c9a2805c0f62a04bacc17c3e
matic.vrscaj <matic.vrscaj@gmail.com>
2019-04-08 22:12:20 +0200

    IIIF support draft, openseadragon

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -23,7 +23,6 @@ $(document).ready(function() {
         prefixUrl: "/images/vendor/openseadragon/",
         tileSources: [
             "http://localhost:8182/iiif/2/entity%2F1001-2000%2F1044%2FIz_zgodovine_Celja-03.pdf/info.json",
-            "http://localhost:8182/iiif/2/entity%2F1001-2000%2F1044%2FIz_zgodovine_Celja-03.pdf/info.json",
         ]
     });
 });
